764818b23adb328a25a1333798ea29e07be2a2d9
Josh Sauder <joshsauder@gmail.com>
2020-06-15 22:11:15 -0400

    added toggle nav

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-scroll";
 import "../sass/navbar.sass"
 
 function Navigation() {
   const [scroll, setScroll] = useState(false);
+  const navRef = useRef(null);
 
   //add scroll event listener
   useEffect(() => window.addEventListener("scroll", setScrollPos), [])
@@ -19,6 +20,9 @@ function Navigation() {
     }
   };
 
+  const toggleNav = () => navRef.current.classList.toggle("hidden");
+  
+
   return (
     <div className="nav-fixed-top">
       <nav className={`flex items-center justify-between flex-wrap p-6 ${scroll ? "bg-darkPurple" : ""}`}>
@@ -26,8 +30,14 @@ function Navigation() {
         <div className="flex items-center flex-shrink-0 text-white mr-6">
             <span className="chewy-font text-xl tracking-tight font-semibold">Josh Sauder</span>
         </div>
+
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-purple-200 border-purple-400 hover:text-white hover:border-white" onClick={toggleNav}>
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+          </button>
+        </div>
       
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto" ref={navRef}>
           <div className="text-sm lg:flex-grow">
             <Link
                 className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4"
